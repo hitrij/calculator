7f70014d53ff8767664e75089832fbbe55b31785
YM <ptic@inbox.lv>
2018-08-23 20:40:07 +0300

    Wrong if expression.length>4

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -8,7 +8,113 @@ package calculator;
 class Calculator {
 
     String calculate(String[] expression) {
-        return "0";
+        System.out.println(expression[0]);
+        System.out.println(expression[1]);
+        System.out.println(expression[2]);
+        System.out.println(expression[3]);
+        System.out.println(expression[4]);
+        Double a = Double.parseDouble(expression[0]);
+        Double b = Double.parseDouble(expression[2]);
+
+        double result = 0;
+
+        switch (expression[1]) {
+            case "+":
+                if (expression.length > 4) {
+                    Double c = Double.parseDouble(expression[4]);
+                    switch (expression[3]) {
+                        case "+":
+                            result = a + b + c;
+                            break;
+                        case "-":
+                            result = a + b - c;
+                            break;
+                        case "*":
+                            result = a + b * c;
+                            break;
+                        case "/":
+                            result = a + b / c;
+                            break;
+                        default:
+                            return "Error";
+                    }
+                }else{
+                    result = a + b;
+                }
+                break;
+            case "*":
+                if (expression.length > 4) {
+                    Double c = Double.parseDouble(expression[4]);
+                    switch (expression[3]) {
+                        case "+":
+                            result = a * b + c;
+                            break;
+                        case "-":
+                            result = a * b - c;
+                            break;
+                        case "*":
+                            result = a * b * c;
+                            break;
+                        case "/":
+                            result = a * b / c;
+                            break;
+                        default:
+                            return "Error";
+                    }
+                }else{
+                    result = a * b;
+                }
+                break;
+            case "/":
+                if (expression.length > 4) {
+                    Double c = Double.parseDouble(expression[4]);
+                    switch (expression[3]) {
+                        case "+":
+                            result = a / b + c;
+                            break;
+                        case "-":
+                            result = a / b - c;
+                            break;
+                        case "*":
+                            result = a / b * c;
+                            break;
+                        case "/":
+                            result = a / b / c;
+                            break;
+                        default:
+                            return "Error";
+                    }
+                }else{
+                    result = a / b;
+                }
+                break;
+            case "-":
+                if (expression.length > 4) {
+                    Double c = Double.parseDouble(expression[4]);
+                    switch (expression[3]) {
+                        case "+":
+                            result = a - b + c;
+                            break;
+                        case "-":
+                            result = a - b - c;
+                            break;
+                        case "*":
+                            result = a - b * c;
+                            break;
+                        case "/":
+                            result = a - b / c;
+                            break;
+                        default:
+                            return "Error";
+                    }
+                }else{
+                    result = a + b;
+                }
+                break;
+            default:
+                return "Error";
+        }
+        return String.valueOf(result);
     }
 
 }
