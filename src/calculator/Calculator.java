39e1aeacd12b21086655e3debce328552ed3db33
YM <ptic@inbox.lv>
2018-08-28 19:47:23 +0300

    Done

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -8,33 +8,31 @@ package calculator;
 class Calculator {
 
     String calculate(String[] expression) {
+
+        for (int i = 1; i < expression.length; i += 2) {
+            String op = expression[i];
+            Double a = Double.parseDouble(expression[i - 1]);
+            Double b = Double.parseDouble(expression[i + 1]);
+            if (op == "*") {
+                expression[i - 1] = String.valueOf(a * b);
+                expression[i] = "+";
+                expression[i + 1] = "0";
+            } else if (op == "/") {
+                expression[i - 1] = String.valueOf(a / b);
+                expression[i] = "+";
+                expression[i + 1] = "0";
+            }
+        }
         double result = Double.parseDouble(expression[0]);
-        //for (int i = 1; i < expression.length; i += 2) {
-          //  String op = expression[i];
-            //Double b = Double.parseDouble(expression[i + 1]);
-            //switch (op) {
-              //  case "*":
-                //    result *= b;
-                  //  break;
-                //case "/":
-                  //  result /= b;
-                    //break;
-                //default:
-                  //  return "Error";
-            //}
-        //}
         for (int i = 1; i < expression.length; i += 2) {
             String op = expression[i];
             Double b = Double.parseDouble(expression[i + 1]);
-            switch (op) {
-                case "+":
-                    result += b;
-                    break;
-                case "-":
-                    result -= b;
-                    break;
-                default:
-                    return "Error";
+            if (op == "+") {
+                result += b;
+            } else if (op == "-") {
+                result -= b;
+            }
+        }
 
         return String.valueOf(result);
     }
