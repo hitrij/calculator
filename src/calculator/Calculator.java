976e0103cdc7b765795ca774a63b7c0d2cde9421
YM <ptic@inbox.lv>
2018-08-28 18:53:50 +0300

    toljko + and -

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -8,112 +8,34 @@ package calculator;
 class Calculator {
 
     String calculate(String[] expression) {
-        System.out.println(expression[0]);
-        System.out.println(expression[1]);
-        System.out.println(expression[2]);
-        if (expression.length > 4) {System.out.println(expression[3]); System.out.println(expression[4]);}
-        Double a = Double.parseDouble(expression[0]);
-        Double b = Double.parseDouble(expression[2]);
+        double result = Double.parseDouble(expression[0]);
+        //for (int i = 1; i < expression.length; i += 2) {
+          //  String op = expression[i];
+            //Double b = Double.parseDouble(expression[i + 1]);
+            //switch (op) {
+              //  case "*":
+                //    result *= b;
+                  //  break;
+                //case "/":
+                  //  result /= b;
+                    //break;
+                //default:
+                  //  return "Error";
+            //}
+        //}
+        for (int i = 1; i < expression.length; i += 2) {
+            String op = expression[i];
+            Double b = Double.parseDouble(expression[i + 1]);
+            switch (op) {
+                case "+":
+                    result += b;
+                    break;
+                case "-":
+                    result -= b;
+                    break;
+                default:
+                    return "Error";
 
-        double result = 0;
-
-        switch (expression[1]) {
-            case "+":
-                if (expression.length > 4) {
-                    Double c = Double.parseDouble(expression[4]);
-                    switch (expression[3]) {
-                        case "+":
-                            result = a + b + c;
-                            break;
-                        case "-":
-                            result = a + b - c;
-                            break;
-                        case "*":
-                            result = a + b * c;
-                            break;
-                        case "/":
-                            result = a + b / c;
-                            break;
-                        default:
-                            return "Error";
-                    }
-                }else{
-                    result = a + b;
-                }
-                break;
-            case "*":
-                if (expression.length > 4) {
-                    Double c = Double.parseDouble(expression[4]);
-                    switch (expression[3]) {
-                        case "+":
-                            result = a * b + c;
-                            break;
-                        case "-":
-                            result = a * b - c;
-                            break;
-                        case "*":
-                            result = a * b * c;
-                            break;
-                        case "/":
-                            result = a * b / c;
-                            break;
-                        default:
-                            return "Error";
-                    }
-                }else{
-                    result = a * b;
-                }
-                break;
-            case "/":
-                if (expression.length > 4) {
-                    Double c = Double.parseDouble(expression[4]);
-                    switch (expression[3]) {
-                        case "+":
-                            result = a / b + c;
-                            break;
-                        case "-":
-                            result = a / b - c;
-                            break;
-                        case "*":
-                            result = a / b * c;
-                            break;
-                        case "/":
-                            result = a / b / c;
-                            break;
-                        default:
-                            return "Error";
-                    }
-                }else{
-                    result = a / b;
-                }
-                break;
-            case "-":
-                if (expression.length > 4) {
-                    Double c = Double.parseDouble(expression[4]);
-                    switch (expression[3]) {
-                        case "+":
-                            result = a - b + c;
-                            break;
-                        case "-":
-                            result = a - b - c;
-                            break;
-                        case "*":
-                            result = a - b * c;
-                            break;
-                        case "/":
-                            result = a - b / c;
-                            break;
-                        default:
-                            return "Error";
-                    }
-                }else{
-                    result = a + b;
-                }
-                break;
-            default:
-                return "Error";
-        }
         return String.valueOf(result);
     }
-
 }
