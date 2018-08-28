a2b90a4d97943f4bb94cd13fabb63cc705adc324
YM <ptic@inbox.lv>
2018-08-28 23:22:11 +0300

    without "()"

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -9,21 +9,24 @@ class Calculator {
 
     String calculate(String[] expression) {
         double result = Double.parseDouble(expression[0]);
-        //for (int i = 1; i < expression.length; i += 2) {
-          //  String op = expression[i];
-            //Double b = Double.parseDouble(expression[i + 1]);
-            //switch (op) {
-              //  case "*":
-                //    result *= b;
-                  //  break;
-                //case "/":
-                  //  result /= b;
-                    //break;
-                //default:
-                  //  return "Error";
-            //}
-        //}
+        double PrevNum;
+        double NextNum;
+
         for (int i = 1; i < expression.length; i += 2) {
+            String op = expression[i];
+            PrevNum = Double.parseDouble(expression[i - 1]);
+            NextNum = Double.parseDouble(expression[i + 1]);
+            if (op == "*") {
+                expression[i + 1] = String.valueOf(PrevNum * NextNum);
+                expression[i] = "+";
+                expression[i - 1] = "0";
+            } else if (op == "/") {
+                expression[i + 1] = String.valueOf(PrevNum / NextNum);
+                expression[i] = "+";
+                expression[i - 1] = "0";
+            }
+        }
+        for (int i = 1;i < expression.length; i += 2) {
             String op = expression[i];
             Double b = Double.parseDouble(expression[i + 1]);
             switch (op) {
@@ -35,7 +38,8 @@ class Calculator {
                     break;
                 default:
                     return "Error";
-
+            }
+        }
         return String.valueOf(result);
     }
 }
